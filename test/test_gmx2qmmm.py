ed5208b5bde06ff65c523e0922f8fd0d77679d2c
Jan-Oliver Joswig <jan.joswig@fu-berlin.de>
2020-10-28 22:36:03 +0100

    PEP8: Black reformatting

---
+++ test/test_gmx2qmmm.py
@@ -0,0 +1,8 @@
+import gmx2qmmm
+import pytest
+
+
+@pytest.mark.parametrize("nested,flattened",
+                         [([1, ["a", "ab"]], [1, "a", "ab"])])
+def test_flatten(nested, flattened):
+    assert flattened == gmx2qmmm._flatten(nested)
