b07aab22325584f9b13846d364806c9605af6393
yuap94 <yuanweipi@gmail.com>
2020-11-02 16:14:15 +0100

    add userInput

---
+++ userInputs.py
@@ -0,0 +1,16 @@
+
+def userInputs():
+	parser = ArgumentParser()
+	parser.add_argument("-c", "--coord", help="coordinate(.g96 or .gro)", type=str, default='conf.g96')
+	parser.add_argument("-p", "--top", help="Topology (.top)", type=str, default='topol.top')
+	from argparse import ArgumentParser
+	parser.add_argument("-n", "--qmatoms", help="QM atoms file(.ndx)", default='qmatoms.ndx')
+	parser.add_argument("-qm", "--qmFile", help="QM parameters (.dat)", type=str, default='qm.dat')
+	parser.add_argument("-mm", "--mmFile", help="MM parameters (.dat)", type=str, default="mm.dat")
+	parser.add_argument("-qmmm", "--qmmmFile", help="QM/MM parameters (.dat)", type=str, default="qmmm.dat")
+	parser.add_argument("-act", "--act", help="Active atoms (.ndx)", type=str, default="act.ndx")
+	parser.add_argument("-path", "--pathFile", help="path file (.dat)", type=str, default="path.dat")
+	parser.add_argument("-g", "--logfile", help=" logfile(.log)", type=str, default="logfile")
+	args = parser.parse_args()
+
+	return args
