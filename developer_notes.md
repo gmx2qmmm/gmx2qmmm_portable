78e5951656f9e5424b23c3104c8067a7276822e2
Jan-Oliver Joswig <31008059+janjoswig@users.noreply.github.com>
2024-10-28 19:19:24 +0100

    WIP: Update install mechanism

---
+++ developer_notes.md
@@ -0,0 +1,8 @@
+Developer notes
+===============
+
+Summary of changes in regards to the migration to v2
+----------------------------------------------------
+
+  * We switched to a `setup.cfg`/`pyproject.toml` based installation mechanism
+  * We switched to a `src/` based package layout
--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup(
-    name='gmx2qmmm',
-    version='0.0.1',
-    packages=setuptools.find_packages(),
-    url='https://github.com/gmx2qmmm/gmx2qmmm_portable/tree/p3',
-    license='gmx2qmmm',
-    author='jangoetze',
-    author_email='gmx2qmmm@gmail.com',
-    description='A python interface for Quantum mechanics/Molecular mechanics (QM/MM) calculation',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    install_requires=['numpy'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+setup()
