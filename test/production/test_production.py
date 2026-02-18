c1db4af2b211d16e5ba70aae4330144728e9b164
den <denis.artyukhin@gmail.com>
2026-02-18 16:58:33 +0100

    switched to pytest; enabled automatic testing(?)

--- test/production/test_production.py
+++ test/production/test_production.py
@@ -1,12 +1,11 @@
 import pytest
 import numpy as np
 from pathlib import Path
-from unittest.mock import patch
 
 from gmx2qmmm.app import App
 from gmx2qmmm.jobs import qm, mm
 
-def test_singlepoint():
+def test_singlepoint(mocker):
 
     current_path = Path(__file__).resolve().parent
     work_dir = current_path / 'test_files'
@@ -15,19 +14,15 @@ def test_singlepoint():
     # Create App instance
     app = App(parameters=str(params_file), work_dir=str(work_dir))
 
-    # Mock the main components
-    # mock_singlepoint = mocker.patch.object("gmx2qmmm.app.Singlepoint")
-    # mock_qm = mocker.patch.object(mock_singlepoint.class_qm_job, 'run_qm_job')
-    # mock_gmx = mocker.patch.object(mock_singlepoint.class_mm_job, 'run_gmx')
+    mocker.patch.object(qm.QM_gaussian, 'run_qm_job')
+    mocker.patch.object(mm.MM, 'run_gmx')
+    mocker.patch.object(mm.MM, 'call_mm_forces')
+    mocker.patch.object(mm.MM, 'call_mm_energy')
+    mocker.patch.object(mm.MM, 'make_gmx_inp')
 
-    with patch.object(qm.QM_gaussian, 'run_qm_job'), \
-         patch.object(mm.MM, 'run_gmx'), \
-         patch.object(mm.MM, 'call_mm_forces'), \
-         patch.object(mm.MM, 'call_mm_energy'), \
-         patch.object(mm.MM, 'make_gmx_inp'):
+    # Run app
+    app.run()
 
-        app.run()
-    
     # compare energies and forces
     oe_file = work_dir / 'oenergy.txt'
     energies = [float(i) for i in open(oe_file).readlines()[-1].split()]
